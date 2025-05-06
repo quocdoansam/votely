0fb1a5b5d1a58f15aeb38c229fb45bf64c8514f3
Sam <quocdoansam.me@gmail.com>
2025-05-06 22:28:20 +0700

    feat: remove unused breadcrumb component and related logic from HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,35 +1,14 @@
 import { ModeToggle } from "@/components/ModeToggle";
 import { AppSidebar } from "@/components/sidebar/AppSidebar";
 import {
-  Breadcrumb,
-  BreadcrumbList,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import {
   SidebarProvider,
   SidebarInset,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { Separator } from "@radix-ui/react-separator";
-import { Link } from "lucide-react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const HomePage = () => {
-  const location = useLocation();
-
-  const breadcrumbMap: Record<string, { label: string; href?: string }[]> = {
-    "/create-election": [
-      { label: "Elections", href: "/" },
-      { label: "Create Election" },
-    ],
-    "/": [{ label: "Home" }],
-  };
-
-  const breadcrumbs = breadcrumbMap[location.pathname] || [];
-
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -39,33 +18,6 @@ const HomePage = () => {
             <div className='flex items-center gap-2 grow'>
               <SidebarTrigger className='-ml-1' />
               <Separator orientation='vertical' className='mr-2 h-4' />
-              {/* <Breadcrumb>
-                <BreadcrumbList>
-                  {breadcrumbs.map((item, index) => (
-                    <>
-                      <BreadcrumbItem
-                        key={`item-${index}`}
-                        className={index === 0 ? "hidden md:block" : ""}
-                      >
-                        {item.href ? (
-                          <BreadcrumbLink asChild>
-                            <Link to={item.href}>{item.label}</Link>
-                          </BreadcrumbLink>
-                        ) : (
-                          <BreadcrumbPage>{item.label}</BreadcrumbPage>
-                        )}
-                      </BreadcrumbItem>
-
-                      {index < breadcrumbs.length - 1 && (
-                        <BreadcrumbSeparator
-                          key={`sep-${index}`}
-                          className={index === 0 ? "hidden md:block" : ""}
-                        />
-                      )}
-                    </>
-                  ))}
-                </BreadcrumbList>
-              </Breadcrumb> */}
             </div>
             <ModeToggle />
           </div>
